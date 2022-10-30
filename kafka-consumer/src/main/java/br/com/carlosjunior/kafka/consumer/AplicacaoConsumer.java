b5ab737cc138896288886104e8c62ba5e15aa802
Carlos Cazelatto Jr <carlosjunior1983@gmail.com>
2022-10-30 08:55:58 -0300

    Add files via upload

---
+++ kafka-consumer/src/main/java/br/com/carlosjunior/kafka/consumer/AplicacaoConsumer.java
@@ -0,0 +1,21 @@
+package br.com.carlosjunior.kafka.consumer;
+
+import br.com.carlosjunior.kafka.consumer.eventos.ConsumidorEvento;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class AplicacaoConsumer {
+
+    public static void main(String[] args) {
+        AplicacaoConsumer aplicacao = new AplicacaoConsumer();
+        aplicacao.iniciar();
+
+    }
+
+    private void iniciar(){
+        log.info("Iniciando a aplicação");
+        ConsumidorEvento consumidor = new ConsumidorEvento();
+        consumidor.executar();
+    }
+
+}
---
+++ kafka-producer/kafka-producer/src/main/java/br/com/carlosjunior/kafka/producer/AplicacaoProducer.java
@@ -0,0 +1,20 @@
+package br.com.carlosjunior.kafka.producer;
+
+import br.com.carlosjunior.kafka.producer.eventos.ProdutorEvento;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class AplicacaoProducer {
+
+    public static void main(String[] args) {
+        AplicacaoProducer aplicacao = new AplicacaoProducer();
+        aplicacao.iniciar();
+
+    }
+
+    private void iniciar(){
+        log.info("Iniciando a aplicação");
+        ProdutorEvento produtor = new ProdutorEvento();
+        produtor.executar();
+    }
+}
